40f126d8df2b8fc4290317c6c304034ff1966df0
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-05-16 16:49:57 +0600

    first commit

---
+++ app/src/main/java/com/axiagroups/remoteapi/data/remote/BeerDto.kt
@@ -0,0 +1,8 @@
+package com.axiagroups.remoteapi.data.remote
+
+
+/**
+ * Created by Ahsan Habib on 5/16/2024.
+ * shehanuk.ahsan@gmail.com
+ */
+data class BeerDto()
---
+++ app/src/main/java/com/axiagroups/remoteapi/data/t.kt
@@ -0,0 +1,9 @@
+package com.axiagroups.remoteapi.data
+
+
+/**
+ * Created by Ahsan Habib on 5/16/2024.
+ * shehanuk.ahsan@gmail.com
+ */
+class t {
+}
